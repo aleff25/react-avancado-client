d6f2c15c1c30f48e3e27d0ff6c415f7d413e43ac
Aleff <aleff2594@gmail.com>
2020-07-23 09:51:15 -0300

    Add feat for boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from './styles';
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, React, NextJs e Styled Components',
+}) => (
+  <S.Container>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para o computador"
+    />
+  </S.Container>
 );
 
 export default Main;
